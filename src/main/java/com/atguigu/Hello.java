c5eb074d7bf093c35cb245fa3f2848a46a85c7be
Syh <Syh@qq.com>
2022-07-28 17:35:33 +0800

    First Commit

---
+++ src/main/java/com/atguigu/Hello.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author syh
+ * @create 2022-07-28 17:33
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
